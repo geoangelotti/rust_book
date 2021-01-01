53ff34f90689f00777656c961c29cdcbd50185b1
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-01-01 20:33:46 +0100

    index out of bounds

--- ch3/3.2/types/src/main.rs
+++ ch3/3.2/types/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
-    let first = a[0];
-    let second = a[1];
+    let index = 10;
+    let element = a[index];
+    println!("The value of element is: {}", element);
 }
