698b94997c3482aaefc92fec4933ee1ee062a143
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-01-02 13:23:07 +0100

    return values

--- ch3/3.3/functions/src/main.rs
+++ ch3/3.3/functions/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let x = 5;
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("The value of y is: {}", y);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
