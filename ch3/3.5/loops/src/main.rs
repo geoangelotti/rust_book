c8f7b09cc752cf37676baabc9822a7eed633a507
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-01-03 17:34:24 +0100

    conditional while loop

--- ch3/3.5/loops/src/main.rs
+++ ch3/3.5/loops/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let mut counter = 0;
+    let mut number = 3;
 
-    let result = loop {
-        counter += 1;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
 
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {}", result);
+    println!("LIFTOFF!!!");
 }
