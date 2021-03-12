8399ce2210ba6547699913c12b7eaef3f5979f9a
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-03-12 12:47:03 +0100

    second mutable borrow does not compile

--- ch4/4.2/borrowing/src/main.rs
+++ ch4/4.2/borrowing/src/main.rs
@@ -6,6 +6,13 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
 }
 
 fn calculate_length(s: &String) -> usize {
