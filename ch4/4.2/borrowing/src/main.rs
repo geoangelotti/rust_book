736ff732e8163b2f53095eaefd7568f3f6e372f2
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-03-12 12:43:01 +0100

    does not compile
    is a `&` reference, so the data it refers to cannot be borrowed as mutable

--- ch4/4.2/borrowing/src/main.rs
+++ ch4/4.2/borrowing/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let s1 = String::from("hellο");
 
+    change(&s1);
+
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
@@ -8,4 +10,8 @@ fn main() {
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", wοrld");
 }
