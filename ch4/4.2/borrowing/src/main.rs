85d1c62aa7c923e49ec3ff4c781f197f563b8222
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-03-12 13:30:01 +0100

    mix mutable with immutable references

--- ch4/4.2/borrowing/src/main.rs
+++ ch4/4.2/borrowing/src/main.rs
@@ -9,15 +9,11 @@ fn main() {
 
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-        r1.push_str(" friεnd");
-        println!("{}", r1);
-    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
 
-    let r2 = &mut s;
-
-    println!("{}", r2);
+    println!("{}, {}, and {}", r1, r2, r3);
 }
 
 fn calculate_length(s: &String) -> usize {
