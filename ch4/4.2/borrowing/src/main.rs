aba9de9f936e813b826d7dc397900a82471780c5
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-03-12 13:30:32 +0100

    not using reference

--- ch4/4.2/borrowing/src/main.rs
+++ ch4/4.2/borrowing/src/main.rs
@@ -11,9 +11,11 @@ fn main() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
 
-    println!("{}, {}, and {}", r1, r2, r3);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
 fn calculate_length(s: &String) -> usize {
