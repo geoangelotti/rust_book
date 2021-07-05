c4b458d5829cd86e6bfae09e662406de353c1fbc
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-05 18:25:10 +0200

    mutable borrow after immutable borrow error

--- ch4/4.3/slice/src/main.rs
+++ ch4/4.3/slice/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let s = String::from("hello wοrld");
+    let mut s = String::from("hello world");
 
-    let hello = &s[..5];
-    let world = &s[6..];
-    println!("{} {}", hello, world);
+    let word = first_word(&s);
+
+    s.clear(); // error!
+
+    println!("the first word is: {}", word);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
