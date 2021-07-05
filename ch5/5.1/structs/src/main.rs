82517492dbf5b2533895fb4d73c4f1f06bc2069c
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-05 20:10:53 +0200

    Tuple Structs

--- ch5/5.1/structs/src/main.rs
+++ ch5/5.1/structs/src/main.rs
@@ -27,4 +27,10 @@ fn main() {
         username: String::from("anotherusername567"),
         ..user1
     };
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
