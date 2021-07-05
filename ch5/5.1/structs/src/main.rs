9e5bdbc4e840b48daefb306121a7e51d466118bb
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-05 18:53:03 +0200

    use rest of other values

--- ch5/5.1/structs/src/main.rs
+++ ch5/5.1/structs/src/main.rs
@@ -25,7 +25,6 @@ fn main() {
     let user2 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
-        active: user1.active,
-        sign_in_count: user1.sign_in_count,
+        ..user1
     };
 }
