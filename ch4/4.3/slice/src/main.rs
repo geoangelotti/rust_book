3217e2ef341fb35310fb271503f43c8bbba58286
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-05 18:29:14 +0200

    String Literals

--- ch4/4.3/slice/src/main.rs
+++ ch4/4.3/slice/src/main.rs
@@ -1,14 +1,21 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
+    // first_word works on slices of `String`s
+    let word = first_word(&my_string[..]);
 
-    s.clear(); // error!
+    let my_string_literal = "hello world";
 
+    // first_word works on slices of string literals
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
     println!("the first word is: {}", word);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
