84e32208cfab01960be479217d6e3cf21ea9d137
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-10 14:14:55 +0200

    more customised enum

--- ch6/6.1/enums/src/main.rs
+++ ch6/6.1/enums/src/main.rs
@@ -4,6 +4,21 @@ fn main() {
         V6(String),
     }
 
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    struct QuitMessage; // unit struct
+    struct MoveMessage {
+        x: i32,
+        y: i32,
+    }
+    struct WriteMessage(String); // tuple struct
+    struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
     let home = IpAddr::V4(127, 0, 0, 1);
 
     let loopback = IpAddr::V6(String::from("::1"));
