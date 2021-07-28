118542a1b1247ec7c702237fcdec5545fe0b72d6
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-28 19:38:26 +0200

    implement enum method

--- ch6/6.1/enums/src/main.rs
+++ ch6/6.1/enums/src/main.rs
@@ -22,4 +22,13 @@ fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
 
     let loopback = IpAddr::V6(String::from("::1"));
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
