30886aa4371be3dbbe6432fa8d98abb9aac1685e
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-07-29 12:05:54 +0200

    anything inside an arm
    even function

--- ch6/6.2/match-flow/src/main.rs
+++ ch6/6.2/match-flow/src/main.rs
@@ -7,12 +7,16 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
     }
 }
+
 fn main() {
-    println!("{}", value_in_cents(Coin::Dime));
+    println!("{}", value_in_cents(Coin::Penny));
 }
