aed0f3307cec7421921266f6b11799cd2e5a4eb8
Geo Angelopoulos <geo.angelotti@gmail.com>
2021-08-08 12:24:56 +0200

    basic vector stuff

---
+++ ch8/8.1/vectors/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    {
+        let v = vec![1, 2, 3, 4];
+
+        // do stuff with v
+    } // <- v goes out of scope and is freed here
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+}
