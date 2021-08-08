0928cc91376be6d8015a52203658422b080a1211
Geo Angelopoulos <geo.angelotti@gmail.com>
2021-08-08 20:02:20 +0200

    mutate strings

--- ch8/8.2/strings/src/main.rs
+++ ch8/8.2/strings/src/main.rs
@@ -1,15 +1,12 @@
 fn main() {
-    let s = String::from("initial contents");
+    let mut s = String::from("foo");
+    s.push_str("bar");
 
-    let hello = String::from("السلام عليكم");
-    let hello = String::from("Dobrý den");
-    let hello = String::from("Hello");
-    let hello = String::from("שָׁלוֹם");
-    let hello = String::from("नमस्ते");
-    let hello = String::from("こんにちは");
-    let hello = String::from("안녕하세요");
-    let hello = String::from("你好");
-    let hello = String::from("Olá");
-    let hello = String::from("Здравствуйте");
-    let hello = String::from("Hola");
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {}", s1);
+
+    let mut s = String::from("lo");
+    s.push('l');
 }
