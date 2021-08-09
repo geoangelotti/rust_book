2bfd551563aceb42d2e244f38005259948519e6a
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 14:15:49 +0200

    use expect for messages

--- ch9/9.2/recoverable/src/main.rs
+++ ch9/9.2/recoverable/src/main.rs
@@ -1,8 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    use std::fs::File;
-
-    let f = File::open("hello2.txt").unwrap();
+    let f = File::open("hello2.txt").expect("Failed to open hello.txt");
 }
