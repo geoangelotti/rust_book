f18498a50e88d3b275ebd7acd45a75e3859e266d
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 20:53:37 +0200

    generic that implements Copy and PartialOrd trait

--- ch10/10.1/generics/src/main.rs
+++ ch10/10.1/generics/src/main.rs
@@ -1,20 +1,23 @@
-enum Option_i32 {
-    Some(i32),
-    None,
-}
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-enum Option_f64 {
-    Some(f64),
-    None,
-}
-
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
     }
+
+    largest
 }
 
 fn main() {
-    let integer = Option_i32::Some(5);
-    let float = Option_f64::Some(5.0);
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
