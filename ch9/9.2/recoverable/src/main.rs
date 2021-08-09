836d0bcbda355a5fa91f4757092c1e8288b1a311
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 14:23:57 +0200

    ? operator should be in Result functions

--- ch9/9.2/recoverable/src/main.rs
+++ ch9/9.2/recoverable/src/main.rs
@@ -1,8 +1,5 @@
-use std::fs;
-use std::io;
+use std::fs::File;
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
+fn main() {
+    let f = File::open("hello.txt")?;
 }
-
-fn main() {}
