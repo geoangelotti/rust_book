aa4a2f752be7b34f96f565523768fb4f957fa9d3
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 20:31:33 +0200

    default implementation of trait

--- ch10/10.2/traits/src/main.rs
+++ ch10/10.2/traits/src/main.rs
@@ -1,7 +1,8 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
-
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
@@ -9,11 +10,7 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
+impl Summary for NewsArticle {}
 
 pub struct Tweet {
     pub username: String,
@@ -37,4 +34,16 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
