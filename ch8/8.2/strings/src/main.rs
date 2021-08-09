5a9756a6c3eac944f0891e7f92912871873eebaf
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 10:04:02 +0200

    cannot index strings

--- ch8/8.2/strings/src/main.rs
+++ ch8/8.2/strings/src/main.rs
@@ -1,18 +1,3 @@
 fn main() {
-    let mut s = String::from("foo");
-    s.push_str("bar");
-
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s1 is {}", s1);
-
-    let mut s = String::from("lo");
-    s.push('l');
-
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
-
-    let s = format!("{}-{}-{}", s1, s2, s3);
-}
+    let s1 = String::from("hello");
+    let h = s1[0];
