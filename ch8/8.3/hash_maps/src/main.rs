48bed9e42f3876615078871daf8ef0068735a1a4
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 10:23:36 +0200

    use fancy iterator magic to create hashmap
    zip 2 iterators aand convert to collection

--- ch8/8.3/hash_maps/src/main.rs
+++ ch8/8.3/hash_maps/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
 }
