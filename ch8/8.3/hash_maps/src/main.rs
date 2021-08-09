cd232d6483a12cef2ffcbf5fbd47d7416fb75a0b
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-09 10:28:03 +0200

    get value from hashmap
    get option more precisely

--- ch8/8.3/hash_maps/src/main.rs
+++ ch8/8.3/hash_maps/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     use std::collections::HashMap;
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    // field_name and field_value are invalid at this point, try using them and
-    // see what compiler error you get!
-    println!("{}-{}", field_name, field_value);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
 }
