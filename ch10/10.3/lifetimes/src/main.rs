a78a72b7f23766b2d09998f713dd071ede01cb82
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-10 13:51:38 +0200

    return lifetime based on code logic

--- ch10/10.3/lifetimes/src/main.rs
+++ ch10/10.3/lifetimes/src/main.rs
@@ -1,16 +1,10 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+    x
 }
 fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
+    let string1 = String::from("abcd");
+    let string2 = "efghijklmnopqrstuvwxyz";
+
+    let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
