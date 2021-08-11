f8df15992a979325aca2c8cd54befaa950670607
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-11 19:36:05 +0200

    search for query

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -22,9 +22,11 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 	let contents = fs::read_to_string(config.filename)?;
 
-	println!("With text:\n{}", contents);
-
 	Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	vec![]
 }
 
 #[cfg(test)]
