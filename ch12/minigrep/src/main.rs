05892bff481b00d05dacf135e24478b9ad2cbc0c
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-11 16:48:27 +0200

    argument parser function

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
 use std::fs;
 
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
