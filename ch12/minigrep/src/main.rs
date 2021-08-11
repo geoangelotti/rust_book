7300d018a77e2ffc95c9c86007b8be89f1845e37
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-11 18:41:11 +0200

    consume config result

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -22,7 +23,10 @@ impl Config {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
