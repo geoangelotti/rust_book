bb6a2452b596a8121603b87500eaa88b7f217774
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-12 15:55:28 +0200

    implement iterator

--- ch13/13.2/counter/src/lib.rs
+++ ch13/13.2/counter/src/lib.rs
@@ -7,3 +7,16 @@ impl Counter {
         Counter { count: 0 }
     }
 }
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
