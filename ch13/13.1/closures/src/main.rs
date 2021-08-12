10fdf9a842ea382d3df1ae7b0f49e99aa1b9b075
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-08-12 13:44:02 +0200

    chacher srtuct to store the closure and the result

--- ch13/13.1/closures/src/main.rs
+++ ch13/13.1/closures/src/main.rs
@@ -1,6 +1,14 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
     let expensive_closure = |num: u32| -> u32 {
         println!("calculating slowly...");
