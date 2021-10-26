78589605d0bfc69c95bfc2b000bf43fb38c80c43
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2021-10-26 15:44:35 +0200

    move v inside the closure

--- ch16/16.1/move_data/src/main.rs
+++ ch16/16.1/move_data/src/main.rs
@@ -3,11 +3,9 @@ use std::thread;
 fn main() {
     let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(|| {
+    let handle = thread::spawn(move || {
         println!("Here's a vector: {:?}", v);
     });
-
-    drop(v); // oh no!
 
     handle.join().unwrap();
 }
